08c3cc1742694a03b3b3a0c13f32b34c51e129c5
BenjaminTan <yiren.tan@gmail.com>
2019-08-14 18:40:23 -0700

    Dry up code

--- simulator.js
+++ simulator.js
@@ -10,6 +10,8 @@ let angle1 = 0;
 let angle2 = 0;
 let angle1Vel = 0;
 let angle2Vel = 0;
+let angle1Acc = 0;
+let angle2Acc = 0;
 let g = 1.0;
 let ballDiameter = 10;
 let dampening = 0.9999;
@@ -33,6 +35,9 @@ let gravitySlider;
 
 // tracking movement
 let buffer;
+
+// dragging state
+let dragging = false;
 
 function setup() {
   document.body.style.background = color(0, 40, 90);
@@ -100,18 +105,6 @@ function draw() {
   text("Gravity: " + g, (gravitySlider.x + gravitySlider.width + 15), (gravitySlider.y + (gravitySlider.height / 2) + (fontSize / 2)));
   text("Dampening: " + `${dampening * 100} %`, (dampeningSlider.x + dampeningSlider.width + 15), (dampeningSlider.y + (dampeningSlider.height / 2) + (fontSize / 2)));
 
-  // calculate angles
-  let angle1Acc = (-g * (2 * mass1 + mass2) * sin(angle1) + -mass2 * g * sin(angle1 - 2 * angle2) + -2 * sin(angle1 - angle2) * mass2 * angle2Vel * angle2Vel * limb2 + angle1Vel * angle1Vel * limb1 * cos(angle1 - angle2)) / (limb1 * (2 * mass1 + mass2 - mass2 * cos(2 * angle1 - 2 * angle2)));
-
-  let angle2Acc = (2 * sin(angle1 - angle2) * (angle1Vel * angle1Vel * limb1 * (mass1 + mass2) + g * (mass1 + mass2) * cos(angle1) + angle2Vel * angle2Vel * limb2 * mass2 * cos(angle1 - angle2))) / (limb2 * (2 * mass1 + mass2 - mass2 * cos(2 * angle1 - 2 * angle2)));
-
-  // calculate ball locations
-  x1 = limb1 * sin(angle1);
-  y1 = limb1 * cos(angle1);
-
-  x2 = x1 + limb2 * sin(angle2);
-  y2 = y1 + limb2 * cos(angle2);
-
   // line stylings
   translate(cx, cy);
   stroke(color(229, 252, 255, 0.3));
@@ -125,15 +118,17 @@ function draw() {
   fill(color(229, 252, 255, 0.3));
   ellipse(x2, y2, ballDiameter, ballDiameter);
 
-  // receive forces
-  angle1Vel += angle1Acc;
-  angle2Vel += angle2Acc;
-  angle1 += angle1Vel;
-  angle2 += angle2Vel;
+  // calculate angles
+  calculateAngles();
+
+  // calculate ball locations
+  calculateBallLocations();
+
+  // calculate forces
+  calculateForces();
 
   // dampening velocity
-  angle1Vel *= dampening;
-  angle2Vel *= dampening;
+  dampenVelocities();
 
   if (frameCount > 1) {
     const transition = map(x2, (limb1 + limb2) * -1, limb1 + limb2, 0, 1);
@@ -145,4 +140,45 @@ function draw() {
 
   prevx2 = x2;
   prevy2 = y2;
+}
+
+function calculateAngles() {
+  if (dragging !== false) {
+    angle1Vel = 0;
+    angle1Acc = 0;
+    angle2Vel = 0;
+    angle2Acc = 0;
+
+    return;
+  } else {
+    angle1Acc = (-g * (2 * mass1 + mass2) * sin(angle1) + -mass2 * g * sin(angle1 - 2 * angle2) + -2 * sin(angle1 - angle2) * mass2 * angle2Vel * angle2Vel * limb2 + angle1Vel * angle1Vel * limb1 * cos(angle1 - angle2)) / (limb1 * (2 * mass1 + mass2 - mass2 * cos(2 * angle1 - 2 * angle2)));
+
+    angle2Acc = (2 * sin(angle1 - angle2) * (angle1Vel * angle1Vel * limb1 * (mass1 + mass2) + g * (mass1 + mass2) * cos(angle1) + angle2Vel * angle2Vel * limb2 * mass2 * cos(angle1 - angle2))) / (limb2 * (2 * mass1 + mass2 - mass2 * cos(2 * angle1 - 2 * angle2)));
+  }
+}
+
+function calculateBallLocations() {
+  // ball 1
+  x1 = limb1 * sin(angle1);
+  y1 = limb1 * cos(angle1);
+
+  // ball 2
+  x2 = x1 + limb2 * sin(angle2);
+  y2 = y1 + limb2 * cos(angle2);
+}
+
+function calculateForces() {
+  // calculate acceleration
+  angle1Vel += angle1Acc;
+  angle2Vel += angle2Acc;
+
+  // add velocity
+  angle1 += angle1Vel;
+  angle2 += angle2Vel;
+}
+
+function dampenVelocities() {
+  // soften velocity
+  angle1Vel *= dampening;
+  angle2Vel *= dampening;
 }
